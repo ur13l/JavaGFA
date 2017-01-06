7fe5f4be23caeb7c7def2d63f41d9b04be5584a2
Uriel Infante <ur13l.infante@gmail.com>
2017-01-06 09:56:59 -0600

    Segundo ejercicio packages

---
+++ 01JavaSE/02/Zoo.java
@@ -0,0 +1,10 @@
+import animales.Animal;
+
+public class Zoo{
+
+  public static void main(String[] args){
+    Animal a = new Animal();
+    a.comer();
+
+  }
+}
---
+++ 01JavaSE/02/animales/Animal.java
@@ -0,0 +1,8 @@
+package animales;
+
+public class Animal{
+
+  public void comer(){
+    System.out.println("El animal está comiendo");
+  }
+}
